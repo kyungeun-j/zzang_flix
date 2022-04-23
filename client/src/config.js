b58e2ecebba3ca87bc59c19334786942b40aa744
kyungeun-j <jjaaaaaang22@gmail.com>
2022-04-24 07:50:34 +0900

    update login

--- client/src/config.js
+++ client/src/config.js
@@ -1,6 +1,5 @@
 import axios from 'axios';
 
 export const axiosInstance = axios.create({
-    // baseURL: "https://zzang-flix.herokuapp.com/api/"
-    baseURL: "http://localhost:4000/api/"
+    baseURL: "https://zzang-flix.herokuapp.com/api/"
 });
--- index.js
+++ index.js
@@ -9,10 +9,10 @@ var cookieParser = require('cookie-parser');
 app.listen(PORT);
 console.log(PORT)
 
-// app.use(express.static(path.join(__dirname, '/client/build')));
-// app.get('*', (req, res) => {
-//   res.sendFile(path.join(__dirname, '/client/build/', 'index.html'));
-// })
+app.use(express.static(path.join(__dirname, '/client/build')));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/client/build/', 'index.html'));
+})
 
 // jwt
 const bodyParser = require('body-parser');
